e81c899eebbed0abb848c7e1f1de3d6e8f7e9688
Adam <adamstirtan@gmail.com>
2021-04-16 08:55:11 -0400

    Added support for Apple Music

---
+++ extension/keysocket-applemusic.js
@@ -0,0 +1,21 @@
+keySocket.init(
+    "applemusic",
+    {
+        "play-pause": function () {
+            var playButton = document.querySelector('button[aria-label="Play"]');
+            var pauseButton = document.querySelector('button[aria-label="Pause"]');
+            if (pauseButton) {
+                keySocket.simulateClick(pauseButton);
+            } else {
+                keySocket.simulateClick(playButton);
+            }
+        },
+        "prev": function(){
+            keySocket.simulateClick(document.querySelector('button[aria-label="Previous"]'));
+        },
+        "next": function(){
+            keySocket.simulateClick(document.querySelector('button[aria-label="Next"]'));
+        }
+        // stop is omitted
+    }
+);
